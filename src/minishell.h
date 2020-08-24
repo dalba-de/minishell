c8fefa7945781b0ce9dff2cdcd08e1c0ceef58cf
agarrido42 <agarridomadrid42@gmail.com>
2020-08-24 17:53:56 +0200

    se añade la estructura de carpetas y se completa la creacion del makefile estructurado

--- src/minishell.h
+++ src/minishell.h
@@ -1,7 +1,7 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include "get_next_line/get_next_line.h"
-# include "libft/libft.h"
+# include <get_next_line/src/get_next_line.h>
+# include <libft/libft.h>
 #include <stdio.h>
 #include <limits.h>
 #include <linux/limits.h>
