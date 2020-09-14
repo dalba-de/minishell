a8917d9d15f8acf5383eff82182090375b1dbc91
agarrido <agarridoMadrid42@gamil.com>
2020-09-15 00:21:35 +0200

    parse_$ modificado

--- src/parser/util_parse/create_strco2.c
+++ src/parser/util_parse/create_strco2.c
@@ -1,6 +1,12 @@
 #include "minishell.h"
 
-char	*parse_dollar(char *str, int start, t_mini *all)
+char	*parse_question(t_mini *all, )
+{
+
+}
+
+
+char	*parse_dollar(char *str, int *start, t_mini *all)
 {
 	int		i;
 	int		j;
@@ -8,7 +14,11 @@ char	*parse_dollar(char *str, int start, t_mini *all)
 	char	*dollar;
 	char	*key;
 	char	*ret;
+	char	*arr;
+	char	*arr2;
 	
+	arr = ft_strcdup(str, '$');
+
 	ret = malloc(sizeof(char *) * 1024);
 	j = -1;
 	while (++j < start)
@@ -17,19 +27,15 @@ char	*parse_dollar(char *str, int start, t_mini *all)
 	start++;
 	j = start;
 	i = 0;
-	if (str[j] == '?')
+
+	if (str[start + 1] == '?')
 	{
 		dollar = ft_itoa(all->exit_status);
-		ft_strncat(ret, dollar, ft_strlen(dollar));
-		i = ft_strlen(dollar) + k + 1;
-		while (str[j])
-		{
-			ret[i] = str[j];
-			i++;
-			j++;
-		}	
-		ret[i] = '\0';
-		return (ret);
+		arr2 = ft_strjoin(arr, dollar);
+		free(dollar);
+		free(arr);
+		arr = arr2;
+		return (arr);
 	}
 	while (str[j] != '$' && str[j] != ' ' && str[j] != '\0')
 	{
@@ -50,9 +56,28 @@ char	*parse_dollar(char *str, int start, t_mini *all)
 	return (ret);
 }
 
+char	*ft_strdupi(char *s, int i)
+{
+	char	*arr;
+	int		cont;
+	
+	arr = malloc((i + 1));
+	cont = 0;
+	while (s[cont] && cont < i)
+	{
+		arr[cont] = s[cont];
+		cont++;
+	}
+	arr[cont] = '\0';
+	return (arr);
+}
+
 char	*create_strco2(t_mini *all, int *cont)
 {
 	char	*str;
+	char	*arr;
+	char	*arr2;
+	char	*arr3;
 	int		i;
 	
 	(*cont)++;
@@ -61,11 +86,28 @@ char	*create_strco2(t_mini *all, int *cont)
 		(*cont)++;
 	all->strl[*cont] == '"' ? ((*cont)++) : 0;
 	i = 0;
+	arr = ft_strdup("");
 	while (str[i])
 	{
 		if (str[i] == '$')
-			str = parse_dollar(str, i, all);
+		{
+			arr2 = ft_strjoin(arr, parse_dollar(&str[i], &i, all));
+			free(arr);
+			arr = arr2;
+		}
+		else
+		{
+			arr3 = ft_strdupi(&str[i], 1);
+			arr2 = ft_strjoin(arr, arr3);
+			free(arr);
+			free(arr3);
+			arr = arr2;
+		}
 		i++;
 	}
+	free(str);
+	str = arr;
 	return (str);
 }
+
+
